38ea33942cd3d30c1a3de65f89199f261bb1d32c
Geeksun2018 <995700257@qq.com>
2022-08-20 02:05:37 +0800

    2022.08.20.1

--- src/main/java/com/geeksun/jvm/rtda/heap/MethodDescriptorParser.java
+++ src/main/java/com/geeksun/jvm/rtda/heap/MethodDescriptorParser.java
@@ -8,6 +8,103 @@ public class MethodDescriptorParser {
     public MethodDescriptorParser(String descriptor){
         this.raw = descriptor;
         this.parsed = new MethodDescriptor();
+        this.startParams();
+        this.parseParamTypes();
+    }
+
+    private void parseParamTypes() {
+        while(true){
+            String t = this.parseFieldType();
+            if(!t.equals("")){
+                this.parsed.addParameterType(t);
+            }else {
+                break;
+            }
+        }
+
+    }
+
+    private void parseReturnType(){
+        if(this.readUint8() == 'V'){
+            this.parsed.returnType = "V";
+            return;
+        }
+        this.unReadUint8();
+        String t = this.parseFieldType();
+        if(t.equals("")){
+            this.parsed.returnType = t;
+            return;
+        }
+        this.causePanic();
+
+    }
+
+    private String parseFieldType(){
+        switch (this.readUint8()){
+            case 'B':
+                return "B";
+            case 'C':
+                return "C";
+            case 'D':
+                return "D";
+            case 'F':
+                return "F";
+            case 'I':
+                return "I";
+            case 'J':
+                return "J";
+            case 'S':
+                return "S";
+            case 'Z':
+                return "Z";
+            case 'L':
+                return this.parseObjectType();
+            case '[':
+                return this.parseArrayType();
+            default:
+                this.unReadUint8();
+                return "";
+                
+        }
+    }
+
+    private String parseArrayType() {
+        return "";
+    }
+
+    private String parseObjectType() {
+        return null;
+    }
+
+    private void startParams() {
+        if(this.readUint8() != '('){
+            this.causePanic();
+        }
+    }
+
+    private void endParams(){
+        if(this.readUint8() != ')'){
+            this.causePanic();
+        }
+    }
+
+    private void finish(){
+        if(this.offset != raw.length()){
+            this.causePanic();
+        }
+    }
+
+    private void causePanic(){
+        System.out.println("panic");
+    }
+
+    private byte readUint8(){
+        byte[] bytes = this.raw.getBytes();
+        return bytes[offset++];
+    }
+
+    private void unReadUint8(){
+        offset--;
     }
 
 
